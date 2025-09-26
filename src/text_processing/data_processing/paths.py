8d2e897b019ce91fb1594141d9bb1b38a73f2fd2
Arthur Maffre <maffrearthur@gmail.com>
2025-09-26 11:58:15 +0200

    Fixing paths

--- src/text_processing/data_processing/paths.py
+++ src/text_processing/data_processing/paths.py
@@ -1,15 +1,20 @@
 from pathlib import Path
 
+# Dossier du fichier courant (paths.py)
+FILE_DIR = Path(__file__).resolve().parent
 
-BASE_DIR = Path.cwd().parents[2]
+# Racine du projet = 3 niveaux au-dessus (adapter si besoin)
+BASE_DIR = FILE_DIR.parents[2]
+
+# Datasets
 DATASET_DIR = BASE_DIR / "src" / "text_processing" / "text_dataset"
 DATASET_OUTPUT_FILE = DATASET_DIR / "text_dataset.parquet"
-SP500_OUTPUT_FILE = DATASET_DIR / "sp500.parquet"
-OUT_FILTER_FILE = DATASET_DIR / "out_filter_dataset.parquet"
-PRE_DATASET_FILE = DATASET_DIR / "pre_dataset.parquet"
-
+SP500_OUTPUT_FILE   = DATASET_DIR / "sp500.parquet"
+OUT_FILTER_FILE     = DATASET_DIR / "out_filter_dataset.parquet"
+PRE_DATASET_FILE    = DATASET_DIR / "pre_dataset.parquet"
 
 if __name__ == "__main__":
+    print("File dir:", FILE_DIR)
     print("Base dir:", BASE_DIR)
     print("Dataset dir:", DATASET_DIR)
     print("Dataset output file:", DATASET_OUTPUT_FILE)
